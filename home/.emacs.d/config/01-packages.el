8e12e5a084c4d02267b52a580a8fa35289050eb1
Jakob Odersky <jakob@odersky.com>
2016-04-02 02:37:31 -0700

    Autoload packages

--- home/.emacs.d/config/01-packages.el
+++ home/.emacs.d/config/01-packages.el
@@ -6,4 +6,9 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(setq use-package-verbose t)
+(setq use-package-always-ensure t)
+
 (require 'use-package)
