0fd5eb903087e77a1eb71efe4b7637f66e7d6851
Jakob Odersky <jakob@odersky.com>
2017-07-13 23:28:08 -0700

    Remove helm, use ivy

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -71,40 +71,19 @@
 
 ;;; Specialized packages
 
-(use-package helm
+(use-package counsel)
+(use-package swiper)
+(use-package ivy
+  :diminish ivy-mode
+  :init
+  (ivy-mode 1)
+  :bind (("C-s" . swiper)
+	 ("M-x" . counsel-M-x)
+	 ("C-x C-f" . counsel-find-file)
+	 ("C-c C-r" . ivy-resume))
   :config
-  (progn ; progn since order is important here
-    (setq helm-candidate-number-limit 100
-	  helm-quick-update t
-	  helm-M-x-requires-pattern nil
-	  helm-M-x-fuzzy-match t
-	  helm-buffers-fuzzy-matching t
-	  helm-recentf-fuzzy-match t
-	  helm-ff-file-name-history-use-recentf t))
-  :bind (("C-h a" . helm-apropos)
-	 ("C-x b" . helm-buffers-list)
-	 ("C-x C-f" . helm-find-files)
-	 ("M-y" . helm-show-kill-ring)
-	 ("M-x" . helm-M-x)
-	 :map helm-map
-	 ("<tab>" . helm-execute-persistent-action)
-	 ("C-i" . helm-execute-persistent-action) ; make TAB works in terminal
-	 ("C-z" . helm-select-action))
-  :diminish helm-mode)
-
-(use-package helm-descbinds
-  :bind ("C-h b" . helm-descbinds))
-
-(use-package helm-swoop
-  :config (setq helm-swoop-pre-input-function
-		(lambda () ""))
-  :bind ("C-s" . helm-swoop))
-
-(use-package helm-projectile
-  :config
-  (helm-projectile-on)
-  :bind (("C-x p" . helm-projectile)
-	 ("C-x P" . helm-projectile-grep)))
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format "(%d/%d) "))
 
 (use-package goto-chg
   :commands goto-last-change
@@ -116,15 +95,17 @@
   :bind ("M-i" . popup-imenu))
 
 (use-package projectile
-  :defer t ; loading is ensured by helm-projectile
   :init
   (setq projectile-use-git-grep t)
   :config
+  (setq projectile-completion-system 'ivy)
   (projectile-global-mode t)
   :diminish projectile-mode)
 
 (use-package magit
-  :bind ("C-x g" . magit-status))
+  :bind ("C-x g" . magit-status)
+  :config
+  (setq magit-completing-read-function 'ivy-completing-read))
 
 (use-package zoom-frm
   :bind (("C-+" . zoom-all-frames-in)
